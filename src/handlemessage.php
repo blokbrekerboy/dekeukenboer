545cf953b7a0d56597e5cf012d1b28b1235ca6f1
Dennis van Riet <dvriet@ictmbo.nl>
2021-09-22 20:05:34 +0200

    db azure

--- src/handlemessage.php
+++ src/handlemessage.php
@@ -14,9 +14,10 @@ if ( ! empty($_POST['name']) && ! empty($_POST['email']) && ! empty($_POST['mess
     // TODO: Check if e-mail is valid by using laminas-validator package: https://packagist.org/packages/laminas/laminas-validator
 
     // When email is valid, add the message to table messages (you can find the sql in keukenboer.sql)
-    $sql = "INSERT INTO message(name, email, message) VALUES (?, ?, ?)";
+
 //    $mysqli = new mysqli('localhost', 'root', 'root', 'keukenboer');
     $mysqli = new mysqli('keukenboer.mysql.database.azure.com', 'keukenboer', 'jhu*ka!QQ23', 'keukenboer');
+    $sql = "INSERT INTO message(name, email, message) VALUES (?, ?, ?)";
     $stmt = $mysqli->prepare($sql);
     $stmt->bind_param("sss", $name, $email, $message);
     if ($stmt->execute()){
